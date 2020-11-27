7f39b98b9d177834e881c4de51fc47afba8dd201
Lê Tấn Trường <57832500+letantruong197@users.noreply.github.com>
2020-11-27 23:01:00 +0700

    update 2

--- blabla.js
+++ blabla.js
@@ -32,19 +32,39 @@ let ans = a.split('').map(function (c) {
         ? c.toLowerCase()
         : c.toUpperCase()
 }).join('')
-function titleCase(){
-console.log(ans)}
+function titleCase() {
+    console.log(ans)
+}
 let pos = prompt("Nhap vi tri mau")
 color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 o = ["th", "st", "nd", "rd"]
-function result(){
-    if(pos == 1){
+function result() {
+    if (pos == 1) {
         cautraloi.innerHTML += "1st choice is " + color[0]
-    }else if(pos == "2"){
+    } else if (pos == "2") {
         cautraloi.innerHTML += "2nd choice is " + color[1]
-    }else if(pos == "3"){
+    } else if (pos == "3") {
         cautraloi.innerHTML += "3rd choice is " + color[2]
-    }else{
+    } else {
         cautraloi.innerHTML += pos + "th choice is " + color[pos - 1]
     }
 }
+function last(arr, n = 1) {
+    if (n > arr.length - 1) {
+        return arr
+    } else {
+        return arr.slice(arr.length - n)
+    }
+}
+function joinArray(arr) {
+    return arr.join(",")
+}
+function sortArray(arr) {
+    var maxValue = Math.max.apply(null, arr);
+    var minValue = Math.min.apply(null, arr);
+    var sortedArray = arr.sort(function (a, b) { return a - b });
+    var reverseArr = sortedArray.reverse();
+    console.log("Max: ", maxValue);
+    console.log("Min: ", minValue);
+    console.log("Array: ", sortedArray);
+    console.log("Reverse Array: ", reverseArr);}
